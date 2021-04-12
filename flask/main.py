f7c1ecf4552e534ea844b8e2af2947c3fdb5a1f4
wasaequreshi <7970246+wasaequreshi@users.noreply.github.com>
2021-04-12 11:11:23 -0700

    Initial commit

---
+++ flask/main.py
@@ -0,0 +1,24 @@
+import os
+import pickle
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+Bootstrap(app)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST' and request.files['file'] != None:
+      SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+      file_path = os.path.join(SITE_ROOT, "model", "model.pickle")
+      classifier = pickle.load(open( file_path, "rb" ))
+      f = request.files['file']
+      return '<p>' + str(classifier.predict(f)) + '</p>'
+   else:
+      return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
