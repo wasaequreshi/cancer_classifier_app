0d55ab268666649883872ecf4bbe30facd746ac5
Ubuntu <ubuntu@ip-172-31-3-162.us-west-2.compute.internal>
2021-04-13 20:56:15 +0000

    Cleaning up comments

--- flask/main.py
+++ flask/main.py
@@ -10,9 +10,8 @@ import numpy
 import nsvision as nv
 import requests
 app = Flask(__name__)
-#freezer = Freezer(app)
-
 Bootstrap(app)
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -21,36 +20,26 @@ def home():
 def upload_file():
    if request.method == 'POST' and request.files['file'].filename != '':
       
-      SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-      file_path = os.path.join(SITE_ROOT, "model", "model.pickle")
-      classifier = pickle.load(open( file_path, "rb" ))
       f = request.files['file'].read()
       f_open = open('./temp', 'wb')
       f_open.write(f)
-    #   data = classifier.predict(f)
-      #data = 0
-      #image = cv2.imdecode(numpy.fromstring(f.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
 
-      #image = cv2.imread(temp, cv2.IMREAD_COLOR)
-      #image_resized = cv2.resize(image, (50, 50), interpolation=cv2.INTER_LINEAR)
-      #image_resized = [[image_resized]]
       image = nv.imread('./temp',resize=(50,50),normalize=True)
-      #image = nv.imread('./temp')
       image = nv.expand_dims(image,axis=0)
+      
       data = json.dumps({"signature_name": "serving_default", "instances": image.tolist()})
       headers = {"content-type": "application/json"}
 
       json_response = requests.post('http://localhost:8502/v1/models/saved_model:predict', data=data, headers=headers)
-      print(json_response)
-      data = int(json_response.json()['predictions'][0][0])
-      print(json_response.json()['predictions'][0]) 
+
       label = ['Malignant','Benign']
+      data = int(json_response.json()['predictions'][0][0])
       data = label[data]
       os.remove('./temp')
+
       return render_template('uploader.html', value=data)
    else:
       return render_template('uploader.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #freezer.freeze()
