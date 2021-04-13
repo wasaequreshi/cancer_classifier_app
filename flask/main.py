e50e8fc494010f8173053f52b64bd9573942d727
Ubuntu <ubuntu@ip-172-31-3-162.us-west-2.compute.internal>
2021-04-13 20:40:34 +0000

    Flask app changes

--- flask/main.py
+++ flask/main.py
@@ -2,10 +2,15 @@ import os
 import pickle
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, request, redirect
-from flask_frozen import Freezer
-
+#from flask_frozen import Freezer
+import json
+from matplotlib.image import imread
+import cv2
+import numpy
+import nsvision as nv
+import requests
 app = Flask(__name__)
-freezer = Freezer(app)
+#freezer = Freezer(app)
 
 Bootstrap(app)
 @app.route('/')
@@ -19,17 +24,33 @@ def upload_file():
       SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
       file_path = os.path.join(SITE_ROOT, "model", "model.pickle")
       classifier = pickle.load(open( file_path, "rb" ))
-      f = request.files['file']
+      f = request.files['file'].read()
+      os.remove('./temp') 
+      f_open = open('./temp', 'wb')
+      f_open.write(f)
     #   data = classifier.predict(f)
-      data = 0
-      if data == 0:
-          data = "Malignant"
-      else:
-          data = "Benign"
+      #data = 0
+      #image = cv2.imdecode(numpy.fromstring(f.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
+
+      #image = cv2.imread(temp, cv2.IMREAD_COLOR)
+      #image_resized = cv2.resize(image, (50, 50), interpolation=cv2.INTER_LINEAR)
+      #image_resized = [[image_resized]]
+      image = nv.imread('./temp',resize=(50,50),normalize=True)
+      #image = nv.imread('./temp')
+      image = nv.expand_dims(image,axis=0)
+      data = json.dumps({"signature_name": "serving_default", "instances": image.tolist()})
+      headers = {"content-type": "application/json"}
+
+      json_response = requests.post('http://localhost:8502/v1/models/saved_model:predict', data=data, headers=headers)
+      print(json_response)
+      data = int(json_response.json()['predictions'][0][0])
+      print(json_response.json()['predictions'][0]) 
+      label = ['Malignant','Benign']
+      data = label[data]
       return render_template('uploader.html', value=data)
    else:
       return render_template('uploader.html')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    freezer.freeze()
+    app.run(debug=True)
+    #freezer.freeze()
